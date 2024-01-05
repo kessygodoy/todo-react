0e82fcd8f1ad7cfbee91a01ae929a21f5957b6c0
Kessy Godoy <kessyon@gmail.com>
2024-01-05 12:58:07 -0300

    Agora é possivel adicionar novas tarefas

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,14 +7,8 @@ function App() {
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
 
-  const initialTodos = [
-    { id: 1, title: 'Estudar React', checked: false },
-    { id: 2, title: 'Estudar ingles', checked: true },
-    { id: 3, title: 'Tocar guitarra', checked: false },
-  ];
-
   const [value, setValue] = useState('');
-  const [todos] = useState(initialTodos);
+  const [todos, setTodos] = useState([]);
 
   const erase = () => {
     setValue('');
@@ -22,6 +16,12 @@ function App() {
 
   const submit = () => {
     console.log('submit', value);
+
+    setTodos([
+      ...todos,
+      { id: new Date().getTime(), title: value, checked: false },
+    ]);
+
     erase();
   };
 
